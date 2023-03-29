1869c8a0ba231bf3d40781d6c70df57ac23f1e8b
root <root@qq.com>
2023-03-29 22:01:32 +0800

    47

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachplanController.java
@@ -0,0 +1,36 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.service.TeachplanService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.*;
+/**
+ * @description 课程计划编辑接口
+ * @author Mr.M
+ * @date 2022/9/6 11:29
+ * @version 1.0
+ */
+
+@RestController
+@CrossOrigin
+@Api(value = "课程计划编辑接口",tags = "课程计划编辑接口")
+public class TeachplanController {
+
+    @Resource
+    TeachplanService teachplanService;
+
+    @ApiOperation("查询课程计划树形结构")
+    @GetMapping("/teachplan/{courseId}/tree-nodes")
+    public List<TeachplanDto> getTreeNodes(@PathVariable Long courseId){
+        return teachplanService.findTeachplanTree(courseId);
+    }
+
+}
