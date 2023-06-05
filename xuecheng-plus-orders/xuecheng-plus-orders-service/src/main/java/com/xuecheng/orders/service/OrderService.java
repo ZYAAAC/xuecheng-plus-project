5a19809e09720603a1234b94090210ed4df18de5
n <aa@qq.com>
2023-06-05 15:58:32 +0800

    152

---
+++ xuecheng-plus-orders/xuecheng-plus-orders-service/src/main/java/com/xuecheng/orders/service/OrderService.java
@@ -0,0 +1,37 @@
+package com.xuecheng.orders.service;
+
+import com.xuecheng.orders.model.dto.AddOrderDto;
+import com.xuecheng.orders.model.dto.PayRecordDto;
+import com.xuecheng.orders.model.dto.PayStatusDto;
+import com.xuecheng.orders.model.po.XcPayRecord;
+
+public interface OrderService {
+
+    /**
+     * @description 创建商品订单
+     * @param addOrderDto 订单信息
+     * @return PayRecordDto 支付记录(包括二维码)
+     */
+    public PayRecordDto createOrder(String userId, AddOrderDto addOrderDto);
+
+    /**
+     * @description 查询支付记录
+     * @param payNo  交易记录号
+     * @return com.xuecheng.orders.model.po.XcPayRecord
+     */
+    public XcPayRecord getPayRecordByPayno(String payNo);
+
+    /**
+     * 请求支付宝查询支付结果
+     * @param payNo 支付记录id
+     * @return 支付记录信息
+     */
+    public PayRecordDto queryPayResult(String payNo);
+
+    /**
+     * @description 保存支付宝支付结果
+     * @param payStatusDto  支付结果信息
+     * @return void
+     */
+    public void saveAliPayStatus(PayStatusDto payStatusDto) ;
+}
